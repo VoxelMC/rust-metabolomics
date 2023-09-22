d14a71e64ec6254b404a45fe7ed1f79a143dbcf7
Voxel <voxelmc@hotmail.com>
2023-09-22 16:03:11 -0700

    📂 | Rearrange into Monorepo

---
+++ data-processing/src/main.rs
@@ -0,0 +1,38 @@
+use polars::prelude::*;
+
+fn main() {
+    use std::fs;
+    use std::path::{Display, Path};
+    println!("Hello, world!");
+
+    use std::env;
+
+    let current_exe = env::current_exe();
+    let current_dir: Display = match current_exe {
+        Ok(ref exe_path) => exe_path.display(),
+        Err(e) => panic!("{e}"),
+    };
+
+    println!("{}", current_dir.to_string());
+
+    let paths = fs::read_dir("./").unwrap();
+
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
+
+    // let file: File = File::open("./Wine Dataset 2023.csv").expect("Could not open this file.");
+
+    let csv_reader = CsvReader::from_path(Path::new("./Wine Dataset 2023.csv"));
+    let df_reader = match csv_reader {
+        Ok(data) => data.truncate_ragged_lines(true).finish(),
+        Err(e) => panic!("{e}"),
+    };
+
+    let df = match df_reader {
+        Ok(data) => data,
+        Err(e) => panic!("{e}"),
+    };
+
+    println!("{df}");
+}
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-}
-
-use polars_core::prelude::*;
-
-let df = CsvReader::from_path("../Wine Dataset 2023.csv").unwrap().finish().unwrap();
-
